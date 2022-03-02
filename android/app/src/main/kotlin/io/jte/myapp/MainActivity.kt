3a7354b5be82ebebdf342e5839b76f43df616559
Joseph TESSIER <joseph.tessier@arondor.com>
2022-03-02 21:13:53 +0100

    First commit

---
+++ android/app/src/main/kotlin/io/jte/myapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.jte.myapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/routes.dart
@@ -0,0 +1,13 @@
+import 'package:myapp/about/about.dart';
+import 'package:myapp/profile/profile.dart';
+import 'package:myapp/home/home.dart';
+import 'package:myapp/topics/topics.dart';
+import 'package:myapp/login/login.dart';
+
+var appRoutes = {
+  '/': (context) => const HomeScreen(),
+  '/login': (context) => const LoginScreen(),
+  '/about': (context) => const AboutScreen(),
+  '/profile': (context) => const ProfileScreen(),
+  '/topics': (context) => const TopicsScreen(),
+};
