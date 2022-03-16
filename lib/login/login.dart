7cc5f4704802fc8acec3036daca8ade471299531
Joseph TESSIER <joseph.tessier@arondor.com>
2022-03-16 16:49:50 +0100

    User authentication

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,10 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:myapp/services/auth.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const FlutterLogo(
+              size: 150,
+            ),
+            Flexible(
+              child: LoginButton(
+                color: Colors.deepPurple,
+                icon: FontAwesomeIcons.userNinja,
+                loginMethod: AuthService().anonLogin,
+                text: 'Continue as guest',
+              ),
+            ),
+            LoginButton(
+              text: 'Sign in with Google',
+              icon: FontAwesomeIcons.google,
+              color: Colors.blue,
+              loginMethod: AuthService().googleLogin,
+            ),
+            // FutureBuilder<Object>(
+            //   future: SignInWithApple.isAvailable(),
+            //   builder: (context, snapshot) {
+            //     if (snapshot.data == true) {
+            //       return SignInWithAppleButton(
+            //         onPressed: () {
+            //           AuthService().signInWithApple();
+            //         },
+            //       );
+            //     } else {
+            //       return Container();
+            //     }
+            //   },
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  final Color color;
+  final IconData icon;
+  final String text;
+  final Function loginMethod;
+
+  const LoginButton({
+    Key? key,
+    required this.text,
+    required this.color,
+    required this.icon,
+    required this.loginMethod,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      child: ElevatedButton.icon(
+        icon: Icon(
+          icon,
+          color: Colors.white,
+          size: 20,
+        ),
+        style: TextButton.styleFrom(
+          backgroundColor: color,
+          padding: const EdgeInsets.all(24),
+        ),
+        onPressed: () => loginMethod(),
+        label: Text(text, textAlign: TextAlign.center),
+      ),
+    );
   }
 }
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 var appTheme = ThemeData(
   bottomAppBarTheme: const BottomAppBarTheme(
     color: Colors.black87,
   ),
+  fontFamily: GoogleFonts.nunito().fontFamily,
   brightness: Brightness.dark,
   textTheme: const TextTheme(
     bodyText1: TextStyle(fontSize: 18),
     bodyText2: TextStyle(fontSize: 16),
-    button: TextStyle(
-      letterSpacing: 1.5,
-      fontWeight: FontWeight.bold,
-      color: Colors.redAccent,
-    ),
+    button: TextStyle(letterSpacing: 1.5, fontWeight: FontWeight.bold, color: Colors.lightGreen),
     headline1: TextStyle(
       fontWeight: FontWeight.bold,
     ),
